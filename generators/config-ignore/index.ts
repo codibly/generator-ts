2c21b4129b7ceeed6c5966893150eba9fab24560
Piotr Oleś <piotr.oles@codibly.com>
2019-06-06 22:05:40 +0200

    fix: 🐛 config-ignore will not miss gitignore file

--- generators/config-ignore/index.ts
+++ generators/config-ignore/index.ts
@@ -3,7 +3,7 @@ import Generator from "yeoman-generator";
 export = class ConfigIgnoreGenerator extends Generator {
   public writing() {
     this.fs.copy(
-      this.templatePath(".gitignore"),
+      this.templatePath("gitignore"),
       this.destinationPath(".gitignore")
     );
   }
